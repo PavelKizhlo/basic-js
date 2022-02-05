3002ec78a3e8408b74be05e8473e4fcf433f7d52
Pavel Kizhlo <pavelkizh@gmail.com>
2022-02-05 16:25:14 +0300

    tasks partially complited

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+  let additionString = '';
+  
+  options.repeatTimes ? options.repeatTimes : options.repeatTimes = 1;
+  options.additionRepeatTimes ? options.additionRepeatTimes : options.additionRepeatTimes = 1;
+  
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    if (options.addition === undefined) break;
+    additionString += options.addition + '     ';
+  }
+
+  additionString = additionString.trim().split('     ').join(options.additionSeparator ? options.additionSeparator : '|');
+  
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result += str + additionString + '     ';
+  }
+  
+  result = result.trim().split('     ').join(options.separator ? options.separator : '+');
+  
+  console.log(result);
+  return result;
 }
+
+let obj =  { repeatTimes: 3, separator: '♥♥♥  ', addition: ' пОкАкУнЬкАл ', additionRepeatTimes: 5, additionSeparator: '( ͡° ͜ʖ ͡°)' };
+
+            //  аГуСиК  пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ♥♥♥  аГуСиК  пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ♥♥♥  аГуСиК  пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл ( ͡° ͜ʖ ͡°) пОкАкУнЬкАл 
+
+ repeater('аГуСиК ', obj)
 
 module.exports = {
   repeater
