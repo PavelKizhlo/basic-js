0d1a1951d9f09499b2e781a1f19be15128d2abe3
Pavel Kizhlo <pavelkizh@gmail.com>
2022-04-27 14:55:49 +0300

    fix: what season pass all tests

--- src/what-season.js
+++ src/what-season.js
@@ -11,22 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
+
 function getSeason(date) {
   if (date === undefined) return 'Unable to determine the time of year!';
 
+  if (isNaN(Date.parse(date))) {
+    throw new Error("Invalid date!");
+  }
+
+  try {
+    date.getUTCMonth();
+  } catch {
+    throw new Error("Invalid date!");
+  }
+
   let month = date.getMonth();
-  
-  if (isNaN(month)) {
-    throw new Error ("Invalid date!");
-  }  // Работает, но не проходит тест
-  
+
   if (month <= 1 || month === 11) return ('winter');
-  if (month >=2 && month <= 4) return('spring');
-  if (month >=5 && month <= 7) return ('summer');
-  if (month >=8 && month <= 10) return ('autumn|fall');
+  if (month >= 2 && month <= 4) return ('spring');
+  if (month >= 5 && month <= 7) return ('summer');
+  if (month >= 8 && month <= 10) return ('autumn|fall');
 
 }
-
 
 module.exports = {
   getSeason
